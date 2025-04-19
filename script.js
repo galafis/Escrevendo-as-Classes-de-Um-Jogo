f28ddeee0c275ef34243227b39d1cba7fda6a174
Gabriel Demetrios Lafis <33261275+galafis@users.noreply.github.com>
2025-04-19 11:29:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// Classe genérica de herói
+class Heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo.toLowerCase();
+  }
+
+  atacar() {
+    let ataque;
+
+    switch (this.tipo) {
+      case "mago":
+        ataque = "magia";
+        break;
+      case "guerreiro":
+        ataque = "espada";
+        break;
+      case "monge":
+        ataque = "artes marciais";
+        break;
+      case "ninja":
+        ataque = "shuriken";
+        break;
+      default:
+        ataque = "um ataque desconhecido";
+    }
+
+    console.log(`O ${this.tipo} atacou usando ${ataque}`);
+  }
+}
+
+// Exemplos de uso
+const heroi1 = new Heroi("Arthas", 30, "guerreiro");
+const heroi2 = new Heroi("Merlin", 150, "mago");
+const heroi3 = new Heroi("Lee", 40, "monge");
+const heroi4 = new Heroi("Hanzo", 28, "ninja");
+
+heroi1.atacar(); // O guerreiro atacou usando espada
+heroi2.atacar(); // O mago atacou usando magia
+heroi3.atacar(); // O monge atacou usando artes marciais
+heroi4.atacar(); // O ninja atacou usando shuriken
